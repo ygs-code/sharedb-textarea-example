edd319e02825afeb297a4f6f7a44df2bbc5cbf79
姚观寿 <yaoguanshou@rainbowcn.com>
2022-03-30 18:25:50 +0800

    源码分析

--- client.js
+++ client.js
@@ -1,6 +1,16 @@
-window.sharedb = require("sharedb/lib/client");
+/*
+ * @Date: 2016-05-25 15:49:21
+ * @Author: Yao guan shou
+ * @LastEditors: Yao guan shou
+ * @LastEditTime: 2022-03-30 18:22:33
+ * @FilePath: /sharedb-textarea-example/client.js
+ * @Description: 
+ */
+var sharedb = require("sharedb/lib/client");
 var ottext = require("ot-text");
-window.ottext = ottext;
 
 var types = require("sharedb/lib/types");
 types.register(ottext.type);
+
+exports.sharedb = sharedb;
+exports.ottext = ottext;
